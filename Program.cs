780a3cec907df6d17bdf7a6ea1f8d0d1f430b4b6
itsmeraju18 <69288937+itsmeraju18@users.noreply.github.com>
2020-08-07 11:06:14 +0530

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,19 +1,12 @@
 ﻿using System;
 
-namespace Arrays
+namespace Exception_handling
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int[] array = new int[5] { 1, 2, 3, 4, 5 };
-            Console.WriteLine(array[0]);
-            Console.WriteLine(array[1]);
-            Console.WriteLine(array[2]);
-            Console.WriteLine(array[3]);
-            Console.WriteLine(array[4]);
-            Console.WriteLine("press enter key to exist");
-            Console.ReadLine();
+            Console.WriteLine("Hello World!");
         }
     }
 }
