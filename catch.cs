1a70bdc03c7a5c9cc9c9de9ac9d476649dde7b82
itsmeraju18 <69288937+itsmeraju18@users.noreply.github.com>
2020-08-07 11:09:14 +0530

    Add files via upload

---
+++ catch.cs
@@ -0,0 +1,25 @@
+﻿//C#: Exception Handling: Handling all exceptions
+using System;
+
+namespace Catch_exception
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+             int x = 0;
+            int div = 0;
+            try
+            {
+                div = 100 / x;
+                Console.WriteLine("Not executed line");
+            }
+            catch
+            {
+                Console.WriteLine("oException");
+            }
+            Console.WriteLine($"Result is {div}");
+            
+        }
+    }
+}
